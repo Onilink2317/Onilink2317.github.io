d0a35fcf5ad01c80b8719a5738582abcf71a2ace
Onilink2222 <onilink2317@gmail.com>
2025-01-05 02:28:45 +0100

    rework

--- assets/js/audio-set.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    let audio = document.getElementById("ost");
-    if (audio) {
-        audio.volume = 0.2;
-        audio.play();
-    }
-});
